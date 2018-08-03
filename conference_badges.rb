aa14ec408fb4327aea63fa2953905c99646fa1cd
Sav Younts <savyounts@gmail.com>
2018-08-03 17:50:10 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,15 @@
 # Write your code here.
+
+def badge_maker(name)
+  puts "Hello, my name is #{name}"
+  
+end 
+
+def batch_badge_creator(names_array)
+  names_array.collect do |name|
+    badge_maker(name)
+  end 
+end 
+
+def assign_rooms(names_array)
+  
